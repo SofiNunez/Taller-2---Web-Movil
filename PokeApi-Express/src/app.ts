a783e83e2b3bdbde295336969d58d6e4f155e44d
Sofia Nuñez <sofia.nunez@alumnos.ucn.cl>
2025-11-16 22:01:59 -0300

    swagger documentation

--- PokeApi-Express/src/app.ts
+++ PokeApi-Express/src/app.ts
@@ -1,15 +1,17 @@
-// src/index.ts
-import express, { Application } from 'express';
+import express from 'express';
 import cors from 'cors';
 import pokemonRoutes from './modules/pokemons/pokemon-route';
-import { PokemonService } from './modules/pokemons/pokemon-service';
 import { initializeDatabase } from './utils/db';
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Rutas
 app.use('/', pokemonRoutes);
